bd25383662c71d340e1f0e113f61129d93e2446d
Rafael Moraes <roliveira.moraes@gmail.com>
2019-10-01 12:13:26 -0300

    New home env configurations

--- .screenlayout/external_monitor.sh
+++ .screenlayout/external_monitor.sh
@@ -1,4 +1,2 @@
 #!/bin/sh
-[ -e "$(which notify-send)" ] && notify-send "Monitor:  External"
-
-xrandr --output HDMI1 --primary --mode 1920x1080 --pos 0x0 --rotate normal --output VIRTUAL1 --off --output eDP1 --off
+xrandr --output HDMI2 --off --output HDMI1 --primary --mode 1920x1080 --pos 0x0 --rotate normal --output DP1 --off --output eDP1 --off --output VIRTUAL1 --off
